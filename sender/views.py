572c73c5a1be573d9d5d29c24e1a5acbc7c70b42
Igor <igor_kichkar@mail.ru>
2022-02-09 14:54:11 +0200

    fixed code status check

--- sender/views.py
+++ sender/views.py
@@ -8,9 +8,9 @@ def send_message(request):
     chat = requests.get('https://dev.wapp.im/v3/chat/spare?crm=TEST&domain=test',
                         headers=tocken)
     chat_json = chat.json()
-    if chat.status_code == '200' and 'token' in chat_json.keys() and 'id' in chat_json.keys():
-        requests.get(f'https://dev.wapp.im/v3/instance{chat_json["id"]}/status?token={chat_json["token"]}',
-                     headers=tocken)
+    if chat.status_code == 200 and 'token' in chat_json.keys() and 'id' in chat_json.keys():
+        inint = requests.get(f'https://dev.wapp.im/v3/instance{chat_json["id"]}/status?token={chat_json["token"]}',
+                             headers=tocken)
         qr_code = requests.get(f'https://dev.wapp.im/v3/instance{chat_json["id"]}/qr_code?token={chat_json["token"]}',
                                headers=tocken)
         return HttpResponse(qr_code.text)
