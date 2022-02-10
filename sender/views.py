8e4492d511e514b294192ec7f1b0379258d92456
Igor <igor_kichkar@mail.ru>
2022-02-10 17:56:44 +0300

    fixed initialization request

--- sender/views.py
+++ sender/views.py
@@ -1,3 +1,4 @@
+import time
 from django.http import HttpResponse
 
 import requests
@@ -5,14 +6,21 @@ import requests
 
 def send_message(request):
     tocken = {'X-Tasktest-Token': 'f62cdf1e83bc324ba23aee3b113c6249'}
-    chat = requests.get('https://dev.wapp.im/v3/chat/spare?crm=TEST&domain=test',
-                        headers=tocken)
+    for i in range(5):
+        chat = requests.get('https://dev.wapp.im/v3/chat/spare?crm=TEST&domain=test',
+                            headers=tocken)
+        if chat.status_code == 200:
+            break
+        else:
+            time.sleep(5)
     chat_json = chat.json()
+    print(chat_json)
     if chat.status_code == 200 and 'token' in chat_json.keys() and 'id' in chat_json.keys():
-        inint = requests.get(f'https://dev.wapp.im/v3/instance{chat_json["id"]}/status?token={chat_json["token"]}',
+        init = requests.get(f'https://dev.wapp.im/v3/instance{chat_json["id"]}/status?token={chat_json["token"]}&full=1',
                              headers=tocken)
         qr_code = requests.get(f'https://dev.wapp.im/v3/instance{chat_json["id"]}/qr_code?token={chat_json["token"]}',
                                headers=tocken)
+        
         return HttpResponse(qr_code.text)
     else:
         return HttpResponse('error')
