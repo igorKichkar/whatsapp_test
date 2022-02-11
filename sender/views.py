8378a2a2bbbea0b93dfb0c38308476116979bfca
Igor <igor_kichkar@mail.ru>
2022-02-11 18:30:26 +0200

    improved code error handler

--- sender/views.py
+++ sender/views.py
@@ -8,35 +8,42 @@ from django.contrib import messages
 from .models import Chat
 from .forms import MessageForm
 
-TOCKEN = {'X-Tasktest-Token': 'f62cdf1e83bc324ba23aee3b113c6249'}
+TOKEN = {'X-Tasktest-Token': 'f62cdf1e83bc324ba23aee3b113c6249'}
 
 
 def send_qr_code(request):
     chat = requests.get('https://dev.wapp.im/v3/chat/spare',
                         params={'crm': 'TEST', 'domain': 'test'},
-                        headers=TOCKEN)
-    if chat.status_code == 200 and 'token' in chat.json().keys() and 'id' in chat.json().keys():
-        requests.get(f'https://dev.wapp.im/v3/instance{chat.json()["id"]}/status',
-                     headers=TOCKEN,
-                     params={'token': chat.json()["token"], 'full': '1'})
+                        headers=TOKEN)
+    if chat.status_code == 200:
+        if 'error_code' in chat.json():
+            messages.error(request, f'Error. {chat.json()["error_text"]}')
+            return render(
+                request,
+                "sender/qr_code.html",
+                {"qr_code": ''},
+            )
+        chat_data = chat.json()
+        requests.get(f'https://dev.wapp.im/v3/instance{chat_data["id"]}/status',
+                     headers=TOKEN,
+                     params={'token': chat_data["token"], 'full': '1'})
         for i in range(30):
-            qr_code = requests.get(f'https://dev.wapp.im/v3/instance{chat.json()["id"]}/qr_code',
-                                   headers=TOCKEN,
-                                   params={'token': chat.json()["token"]})
+            qr_code = requests.get(f'https://dev.wapp.im/v3/instance{chat_data["id"]}/qr_code',
+                                   headers=TOKEN,
+                                   params={'token': chat_data["token"]})
             if qr_code.text:
                 break
-            else:
-                time.sleep(1)
-        Chat.objects.create(instance_id=int(chat.json()["id"]), tocken=chat.json()["token"])
+            time.sleep(1)
+        Chat.objects.create(instance_id=int(chat_data["id"]), tocken=chat_data["token"])
         messages.success(request, 'Ok.')
         qr_code_text = qr_code.text
     else:
-        messages.error(request, f'Error. {chat.json()["error_text"] if chat.status_code == 200 else chat.status_code}')
+        messages.error(request, f'Error. {chat.status_code}')
         qr_code_text = ''
     return render(
         request,
         "sender/qr_code.html",
-        {"qr_code": qr_code_text},
+        {"qr_code": qr_code_text}
     )
 
 
@@ -50,10 +57,10 @@ def send_message(request):
         }
 
         message = requests.post(f'https://dev.wapp.im/v3/instance{chat.instance_id}/sendMessage',
-                                headers=TOCKEN,
+                                headers=TOKEN,
                                 params={'token': chat.tocken},
                                 data=data)
-        if message.status_code == 200 and 'sent' in message.json().keys():
+        if message.status_code == 200 and 'sent' in message.json():
             messages.success(request, 'Ok. Message sent.')
         else:
             messages.error(request, 'Error. Message not sent.')
